65cde937dc0ed29bae4309676a1b483ff4b8cc23
Rely Ky <relyky@hotmail.com>
2024-07-13 16:30:51 +0800

    RxDemo1 - II

---
+++ N8WasmMudLab/Models/MyObserver.cs
@@ -0,0 +1,27 @@
+﻿using System.Reactive;
+using System.Reactive.Linq;
+
+namespace N8WasmMudLab.Models;
+
+internal class MyObserver<T> : IObserver<T>
+{
+  public MyObserver()
+  {
+    
+  }
+
+  void IObserver<T>.OnNext(T value)
+  {
+    Console.WriteLine($"IObserver<{nameof(T)}>.OnNext => {value}");
+  }
+
+  void IObserver<T>.OnCompleted()
+  {
+    Console.WriteLine($"IObserver<{nameof(T)}>.OnCompleted");
+  }
+
+  void IObserver<T>.OnError(Exception ex)
+  {
+    Console.WriteLine($"IObserver<{nameof(T)}>.OnError! " + ex.Message);
+  }
+}
