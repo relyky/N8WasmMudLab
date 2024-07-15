562564a1730c8affa5c5218066dfb8a709b3c0d2
Rely Ky <relyky@hotmail.com>
2024-07-15 14:51:03 +0800

    RxDemo1 - V

--- N8WasmMudLab/Models/MyObserver.cs
+++ N8WasmMudLab/Models/MyObserver.cs
@@ -1,27 +1,46 @@
-﻿using System.Reactive;
+﻿using Microsoft.AspNetCore.Components;
+using System.Reactive;
 using System.Reactive.Linq;
 
 namespace N8WasmMudLab.Models;
 
 internal class MyObserver<T> : IObserver<T>
 {
-  public MyObserver()
+  string name;
+  public Queue<string> MsgQueue { get; private set; } = new Queue<string>(10);
+  public int NextCnt { get; private set; } = 0;
+
+  /// <summary>
+  /// 通告外部有新訊息
+  /// </summary>
+  public Action<T>? OnNextReceived = null;
+
+  public MyObserver(string name)
   {
-    
+    this.name = name; 
   }
 
   void IObserver<T>.OnNext(T value)
   {
-    Console.WriteLine($"IObserver<{nameof(T)}>.OnNext => {value}");
+    NextCnt++;
+    string msg = $"{name}: {value} at {DateTime.Now:mmss.fff}";
+
+    //# 只保留最後10筆訊息。
+    if(MsgQueue.Count >= 10) MsgQueue.Dequeue();
+    MsgQueue.Enqueue(msg);
+    //Console.WriteLine(msg);
+
+    //# 通告外部有新訊息。
+    OnNextReceived?.Invoke(value);
   }
 
   void IObserver<T>.OnCompleted()
   {
-    Console.WriteLine($"IObserver<{nameof(T)}>.OnCompleted");
+    //Console.WriteLine($"{name}.OnCompleted");
   }
 
   void IObserver<T>.OnError(Exception ex)
   {
-    Console.WriteLine($"IObserver<{nameof(T)}>.OnError! " + ex.Message);
+    //Console.WriteLine($"{name}.OnError! " + ex.Message);
   }
 }
